9f27f835cd04896e83827ce6794e4a0c6b1d17da
hccode0419 <67219037+hccode0419@users.noreply.github.com>
2024-05-08 11:28:36 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -2,16 +2,17 @@ n, m = map(int, input().split())
 
 nums = list(map(int, input().split()))
 
+nums.reverse()
 
-nums = sorted(nums)
 
-L = []
+mx = 0
+
 for i in range(n):
     for j in range(i+1 , n):
         for k in range(j+1, n):
             sum = nums[i] + nums[j] + nums[k]
             if sum <= m:
-                L.append(sum)
-                     
-
-print(max(L))
+                if mx < sum:
+                    mx = sum                     
+                
+print(mx)
