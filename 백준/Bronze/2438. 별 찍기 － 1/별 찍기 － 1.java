ed737efb84b76ea4bfd38544c6b06b6381b8406c
hccode0419 <67219037+hccode0419@users.noreply.github.com>
2024-10-06 20:05:05 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 236 ms, Memory: 17948 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
 
 class StarPrinter {
-    // 생성자
     public StarPrinter() {}
 
-    // 메서드: 입력된 숫자만큼 별을 출력하는 메서드
     public void printStars(int n) {
-        for (int i = 1; i <= n; i++) {
-            // i번째 줄에 i개의 별 출력
-            for (int j = 1; j <= i; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
-            System.out.println(); // 개행
+            System.out.println();
         }
     }
 }
 
-public class Main {  // 클래스 이름을 Main으로 수정
+public class Main {
     public static void main(String[] args) {
-        // 사용자로부터 입력 받기
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        // StarPrinter 객체 생성 및 별 출력
         StarPrinter starPrinter = new StarPrinter();
         starPrinter.printStars(n);
 
